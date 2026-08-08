1820813d37d9ce96e44e2f0c632f3f73d5a90b50
Hiba Fathima <hibafthmaa@gmail.com>
2026-08-08 21:44:33 +0530

    create calculator HTML structure

--- index.js
+++ index.js
@@ -15,3 +15,16 @@ function div(a,b) {
 let firstNumber;
 let secondNumber;
 let operator;
+
+function operate( firstNumber,secondNumber, operator) {
+    if (operator === "+") {
+        return add(firstNumber, secondNumber);
+    }else if (operator==="-"){
+        return sub(firstNumber,secondNumber);
+    }else if (operator==="*"){
+        return mul(firstNumber,secondNumber);
+    }else if (operator==="/"){
+        return div(firstNumber,secondNumber);
+    }
+}
+console.log(operate);
